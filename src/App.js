61f38935dc52ff492aa80a05f9254f4b9baf59d8
Karim Badwork <karim@badwork.fr>
2021-09-16 22:07:30 +0200

    404 page and router redirect

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect,
   Link
 } from "react-router-dom";
 import Confidentialite from './pages/confidentialite'
 import Rgpd from './pages/rgpd'
 import Legal from './pages/legal'
 import Home from './pages/home'
+import NoMatch from './pages/nomatch'
 import Header from './global/header'
 import Footer from './global/footer'
 import logo from './logo.svg';
@@ -25,6 +27,10 @@ function App() {
             <Route path="/mentions-legales" exact component={Legal} />
             <Route path="/confidentialite" exact component={Confidentialite} />
             <Route path="/rgpd" exact component={Rgpd} />
+            <Route path="/404" exact component={NoMatch} />
+            <Route path="*">
+              <Redirect to="/404" />
+            </Route>
         </Switch>
         <Footer />
       </Router>
---
+++ src/pages/nomatch.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NoMatch = () => {
+    return (
+        <div className="nomatch">
+            <h1>404<span> - page not found</span></h1>
+        </div>
+    )
+}
+
+export default NoMatch
