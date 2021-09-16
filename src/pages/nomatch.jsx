4cbc9b3ba38c8b0548066a66eb41c69868218985
Karim Badwork <karim@badwork.fr>
2021-09-16 22:19:29 +0200

    404 finish responsive btn home

--- src/pages/nomatch.jsx
+++ src/pages/nomatch.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import {
+    Link
+  } from "react-router-dom";
 
 const NoMatch = () => {
     return (
         <div className="nomatch">
-            <h1>404<span> - page not found</span></h1>
+            <h1>404<span> <span>-</span> page not found</span></h1>
+            <Link to="/">Home</Link>
         </div>
     )
 }
